a1187bf9b4319b79770e8589fee9ea7fe4ef099d
João Ricardo <joaoricardodesousamatos@gmail.com>
2023-05-10 10:51:29 -0300

    Criando menu de naveação

--- App.js
+++ App.js
@@ -1,33 +1,69 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { CountdownCircleTimer } from 'react-native-countdown-circle-timer'
-import { useCountdown } from 'react-native-countdown-circle-timer'
+import { StyleSheet, Text, View, Button, TouchableOpacity, Image, SafeAreaView } from 'react-native';
+/* import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';*/
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Descanço de 1:10</Text>
-      
-      <CountdownCircleTimer
-      isPlaying
-      duration={70}
-      colors={['#004777']}
-      size = '40'
-      strokeWidth={3}
-      >
-    {({ remainingTime }) => <Text>{remainingTime}</Text>}
-    </CountdownCircleTimer>
-    <button>{remainingTime}</button>
+    <SafeAreaView style={{flex: 1, backgroundColor: '#76d600'}}>
+      <View style={styles.menu}>
+        <TouchableOpacity style={styles.navegar} activeOpacity={0.5}>
+          <Image
+          source={{uri: 'https://cdn-icons-png.flaticon.com/512/60/60817.png'}}
+          style={styles.buttonIcon}
+          />
+         <View style={styles.separator}/>
+         <Image
+         source={{uri: 'https://cdn-icons-png.flaticon.com/512/60/60817.png'}}
+         style={styles.buttonIcon}
+         />
+         <View style={styles.separator}/>
+         <Image
+         source={{uri: 'https://cdn-icons-png.flaticon.com/512/60/60817.png'}}
+         style={styles.buttonIcon}
+         />
+       </TouchableOpacity>
+      <View style={styles.separator}/>
+      <TouchableOpacity style={styles.navegar} activeOpacity={0.5}>
+        <Image
+        source={{uri: 'https://cdn-icons-png.flaticon.com/512/60/60817.png'}}
+        style={styles.buttonIcon}
+        />
+      </TouchableOpacity>
       <StatusBar style="auto" />
-    </View>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  menu: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#76d600',
     alignItems: 'center',
     justifyContent: 'center',
+    flexDirection: 'row',
+  },
+  navegar: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#485a96',
+    borderWidth: 0.5,
+    borderColor: '#fff',
+    height: 40,
+    borderRadius: 5,
+    margin: 5,
+  },
+  separator: {
+    backgroundColor: '#fff',
+    width: 1,
+    height: 40,
+  },
+  buttonIcon: {
+    padding: 10,
+    margin: 5,
+    height: 25,
+    width: 25,
+    resizeMode: 'stretch'
   },
 });
