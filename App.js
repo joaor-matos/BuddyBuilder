f87eab6f5f98ce5470ac2fc7a1943046409dff86
joaor-matos <joaoricardodesousamatos@gmail.com>
2023-06-03 17:42:41 -0300

    Cronômetro adicionado

--- App.js
+++ App.js
@@ -1,21 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button, TouchableOpacity, Image, SafeAreaView, ScrollView } from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { startTransition } from 'react';
-// import { Stopwatch, Timer } from 'react-native-stopwatch-timer';
-
-function ConfigScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Configuração</Text>
-    </View>
-  );
-}
+import Cronometro from './Cronometro';
 
 function HomeScreen({ navigation }) {
   return (
-    <SafeAreaView style={{flex: 1, backgroundColor: '#76d600', flexDirection: 'column'}}>
+    <SafeAreaView style={{flex: 1, backgroundColor: '#76d600', flexDirection: 'column' }}>
       <View style={styles.menu}>
       <TouchableOpacity style={styles.bttnConfig} activeOpacity={0.9} 
       onPress={() => navigation.navigate('Config')}>
@@ -25,15 +17,6 @@ function HomeScreen({ navigation }) {
         />
       </TouchableOpacity>
       <View style={styles.separator}/>
-      <TouchableOpacity style={styles.bttnHome} activeOpacity={0.9}>
-        <Image
-        source={require('./images/home.png')}
-        style={styles.buttonIcon}
-        />
-      </TouchableOpacity>
-
-      <View style={styles.separator}/>
-
       <TouchableOpacity style={styles.bttnTreino} activeOpacity={0.9}>
         <Image
         source={require('./images/treino.png')}
@@ -46,11 +29,23 @@ function HomeScreen({ navigation }) {
       </View>
       <View style={styles.viewTreino}>
         <ScrollView style={styles.scrollTreino}>
-      
+          <Exerc/>
         </ScrollView>
+    </View>
+    <View style={{flexDirection: 'row', justifyContent: 'space-around', flex: 0.13,}}>
+    <Cronometro/>
+    <Cronometro/>
     </View>
     </SafeAreaView>
 
+  );
+}
+
+function ConfigScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Configuração</Text>
+    </View>
   );
 }
 
@@ -61,21 +56,26 @@ function App() {
     <NavigationContainer>
       <Stack.Navigator initialRouteName='Home'>
         <Stack.Screen options={{headerShown: false}} name='Home' component={HomeScreen}/>
-        <Stack.Screen options={{
-            title: <Text style={{fontSize: 10}}>Configurações</Text>, headerStyle:{height: 30, justifyContent: 'center'}}} name='Config' component={ConfigScreen}/>
+        <Stack.Screen options={{headerShown: false}} name='Config' component={ConfigScreen}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
+export const Exerc = (props) => (
+    <View style={styles.viewExerc}>
+      <View style={styles.sepExerc} />
+      
+    </View>
+);
+
 export default App;
 
 const styles = StyleSheet.create({
   menu: {
-    flex: 0.10,
-    padding: 10,
+    flex: 0.2,
+    alignItems: 'center',
     backgroundColor: '#76d600',
-    alignItems: '',
     justifyContent: 'center',
     flexDirection: 'row',
   },
@@ -85,27 +85,25 @@ const styles = StyleSheet.create({
     backgroundColor: 'black',
     borderWidth: 0.5,
     borderColor: '#white',
-    height: 40,
+    height: 80,
     borderRadius: 5,
     margin: 5,
   },
   bttnHome: {
     flexDirection: 'row',
     alignItems: 'center',
-    height: 40,
-    width: 50,
+    height: 70,
+    width: 100,
     backgroundColor: '#D9D9D9',
-
     borderColor: '#fff',
-    height: 40,
   },
   bttnConfig: {
     flexDirection: 'row',
     alignItems: 'center',
     backgroundColor: '#D9D9D9',
     borderColor: '#fff',
-    height: 40,
-    width: 50,
+    height: 70,
+    width: 100,
     borderTopLeftRadius: 5,
     borderBottomLeftRadius: 5,
   },
@@ -114,22 +112,22 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#D9D9D9',
     borderColor: '#fff',
-    height: 40,
-    width: 50,
+    height: 70,
+    width: 100,
     borderTopRightRadius: 5,
     borderBottomRightRadius: 5,
   },
   separator: {
     backgroundColor: '#636363',
-    width: 0.5,
-    height: 40,
+    width: 1,
+    height: 70,
   },
   buttonIcon: {
     padding: 13,
     marginLeft: 'auto',
     marginRight: 'auto',
-    height: 25,
-    width: 25,
+    height: 50,
+    width: 50,
     resizeMode: 'stretch',
     justifyContent: 'center',
   },
@@ -137,21 +135,41 @@ const styles = StyleSheet.create({
     padding: 15,
     marginLeft: 'auto',
     marginRight: 'auto',
-    height: 25,
-    width: 25,
+    height: 50,
+    width: 50,
     resizeMode: 'stretch',
     justifyContent: 'center',
     transform: [{rotate: '-20deg'}],
   },
   viewTreino: {
+    flex: 0.3,
+    alignSelf: 'center',
     backgroundColor: '#D9D9D9',
     marginHorizontal: 'auto',
-    justifyContent: 'flex-start',
-    borderRadius: 6,
-    height: 100,
-    width: 200,
+    justifyContent: 'center',
+    borderRadius: 10,
+    width: 360,
     alignItems: 'center',
     flexDirection: 'column',
-    paddingVertical: 2,
-  }
+  },
+  viewExerc: {
+    marginTop: 10,
+    backgroundColor: '#636363',
+    height: 70,
+    width: 340,
+    borderRadius: 10,
+  },
+  sepExerc: {
+    backgroundColor: 'black',
+    width: 3,
+    height: 70,
+    marginLeft: 40,
+  },
+  divExerc: {
+    backgroundColor: 'black',
+    width: 300,
+    height: 3,
+    marginTop: 30,
+    marginLeft: 40,
+  },
 });
