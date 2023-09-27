c650e1d35d321d787de8c703363f6492205d79e8
Pedro Soares Dias <pedroferraridias@gmail.com>
2023-09-27 09:28:19 -0300

    Criado a página de Criar Treino

---
+++ pages/CriarTreino.js
@@ -0,0 +1,60 @@
+import { useState } from "react";
+import { View, TextInput, TouchableOpacity, StyleSheet, Image } from "react-native";
+
+export default function CriarTrieno() {
+    const [nomeTreino, setNomeTreino] = useState("");
+    const [exercicio, setExercicio] = useState("");
+    const [quantRepeticao, setQuantRepeticao] = useState(0);
+    const [quantSets, setQuantSets] = useState(0);
+    const [descricao, setDescricao] = useState("");
+
+    const handleCreate = () => {
+        // Criar a lógica do botão de criar novas listas
+    }
+
+    const handleFinally = () => {
+        // Criar a lógica do botão para finalizar a criação dos treinos
+    }
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.treino}>
+                <TextInput style={styles.inputTreino} value={nomeTreino} keyboardType="text" onChange={(text) => setNomeTreino(text)} placeholder="Nome do Treino" />
+            </View>
+            <View>
+                <TextInput value={exercicio} onChange={(text) => setExercicio(text)} keyboardType="text" placeholder="Exercício" />
+                <TextInput value={quantRepeticao} onChange={(text) => setQuantRepeticao(text)} keyboardType="number" placeholder="Repetições" />
+                <TextInput value={quantSets} onChange={(text) => setQuantSets(text)} keyboardType="number" placeholder="Sets" />
+                <TextInput value={descricao} onChange={(text) => setDescricao(text)} keyboardType="text" placeholder="Descrição" />
+            </View>
+
+            <TouchableOpacity onPress={handleCreate}><Image source={"../svg/plus.svg"} alt="Adicionar exercício"/></TouchableOpacity>
+
+            <TouchableOpacity onPress={handleFinally}>Finalizar</TouchableOpacity>
+
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 0.5,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 20,
+    },
+    treino: {
+        flex: 0.5,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 10
+    },
+    inputTreino: {
+        color: "#000000",
+        fontSize: 20,
+        borderRadius: 10,
+        padding: 10,
+        marginBottom: 10,
+        marginHorizontal: 2
+    }
+})
