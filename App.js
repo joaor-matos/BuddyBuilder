12f47f637581e0bf63249a4bb7478d6b9d30f024
joaor-matos <dev.joaormatos@gmail.com>
2024-05-27 00:35:44 -0300

    Contagem de tempo de treino implementada

--- App.js
+++ App.js
@@ -20,7 +20,7 @@ function App() {
 
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Login'>
+      <Stack.Navigator initialRouteName='Treino'>
         <Stack.Screen options={{ headerShown: false }} name='Home' component={Home} />
         <Stack.Screen options={{ headerShown: false }} name='Treino' component={Treino} />
         <Stack.Screen options={{ headerShown: false }} name='Configuracao' component={ConfigScreen} />
--- pages/Treino.js
+++ pages/Treino.js
@@ -2,65 +2,132 @@ import React, { Suspense } from "react";
 import { StyleSheet, Text, View, TouchableOpacity, Image, SafeAreaView, ScrollView, ActivityIndicator } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import { useRoute } from "@react-navigation/native";
+import { useState, useEffect } from 'react';
 
 import Cronometro from '../components/Cronometro';
 import Exercicio from '../components/Exercicio';
 import Temporizador from '../components/Temporizador';
 
 function Treino({ navigation }) {
-  const route = useRoute();
-  const { exercicios } = route.params;
+  // const route = useRoute();
+  // const { exercicios } = route.params;
 
-  if (!exercicios || exercicios.length === 0) {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Nenhum exercício encontrado para este treino.</Text>
-      </View>
-    );
-  }
+  // if (!exercicios || exercicios.length === 0) {
+  //   return (
+  //     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+  //       <Text>Nenhum exercício encontrado para este treino.</Text>
+  //     </View>
+  //   );
+  // }
+
+  // Função de cronometrar tempo de treino
+
+  const [segundos, setSegundos] = useState(0);
+  const [minutos, setMinutos] = useState(0);
+  const [horas, setHoras] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+
+  const [buttonColor, setButtonColor] = useState('#549E48');
+
+  useEffect(() => {
+    let intervalId;
+
+    if (isRunning) {
+      intervalId = setInterval(() => {
+        setSegundos(segundos => segundos + 1);
+      }, 1000);
+    }
+
+    return () => clearInterval(intervalId);
+  }, [isRunning]);
+
+  const formatTime = value => {
+    return value < 10 ? `0${value}` : value;
+  };
+
+  const startTimer = () => {
+    if (isRunning === false) {
+      setIsRunning(true)
+      const newColor = buttonColor === '#549E48' ? '#AB0000' : '#549E48';
+    setButtonColor(newColor);
+    } else if (isRunning === true) {
+      setIsRunning(false)
+      const newColor = buttonColor === '#AB0000' ? '#549E48' : '#AB0000';
+    setButtonColor(newColor);
+    }
+    ;
+  };
+
+  const stopTimer = () => {
+    
+    setIsRunning(false);
+  };
+
+  useEffect(() => {
+    if (segundos === 60) {
+      setSegundos(0);
+      setMinutos(minutos => minutos + 1);
+    }
+
+  }, [segundos, minutos]);
+
+  useEffect(() => {
+    if (minutos === 60) {
+      setMinutos(0);
+      setHoras(Horas => horas + 1);
+    }
+
+  }, [segundos, minutos]);
 
   return (
-    <Suspense fallback={(
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <ActivityIndicator size="large" color="#0000FF" />
-      </View>)}
-    >
-      <SafeAreaView style={{ flex: 1, backgroundColor: '#182649', flexDirection: 'column' }}>
-        <View style={styles.menu}>
-          <TouchableOpacity style={styles.bttnMenu} activeOpacity={0.9}>
-            <View style={{ backgroundColor: '#549E48', height: '60%', width: '60%', borderRadius: 6, }} />
-          </TouchableOpacity>
+    // <Suspense fallback={(
+    //   <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    //     <ActivityIndicator size="large" color="#0000FF" />
+    //   </View>)}
+    // >
 
-          <TouchableOpacity style={styles.timerTreino} activeOpacity={0.9}>
-            <Text style={{ fontSize: 35, fontWeight: 'bold', marginHorizontal: 10 }}>0:00:00</Text>
-          </TouchableOpacity>
 
-          <TouchableOpacity style={styles.bttnMenu} activeOpacity={0.9}
-            onPress={() => navigation.navigate('Configuracao')}>
-            <Image
-              source={require('../images/menu.png')}
-              style={styles.iconMenu}
-            />
-          </TouchableOpacity>
+    <SafeAreaView style={{ flex: 1, backgroundColor: '#182649', flexDirection: 'column' }}>
+      <View style={styles.menu}>
+        <TouchableOpacity style={styles.bttnMenu} activeOpacity={0.9} onPress=
+        
+        {startTimer}> 
+        
+          <View style={[styles.btnTimer, { backgroundColor: buttonColor}]} />
+        </TouchableOpacity>
 
-          <StatusBar style="auto" />
-
+        <View style={styles.timerTreino}>
+          <Text style={{ fontSize: 35, fontWeight: 'bold', marginHorizontal: 10 }}>
+            {formatTime(horas)}:{formatTime(minutos)}:{formatTime(segundos)}
+            </Text>
         </View>
-        <View style={styles.viewTreino}>
-          <ScrollView style={styles.scrollTreino}>
-            {exercicios.map((exercicio) => 
+
+        <TouchableOpacity style={styles.bttnMenu} activeOpacity={0.9}
+          onPress={() => navigation.navigate('Configuracao')}>
+          <Image
+            source={require('../images/menu.png')}
+            style={styles.iconMenu}
+          />
+        </TouchableOpacity>
+
+        <StatusBar style="auto" />
+
+      </View>
+      <View style={styles.viewTreino}>
+        <ScrollView style={styles.scrollTreino}>
+          {/* {exercicios.map((exercicio) => 
               <Exercicio key={exercicio?.id} exercicio={exercicio} />
-            )}
-          </ScrollView>
-        </View>
+            )} */}
+        </ScrollView>
+      </View>
 
-        <View style={{ flexDirection: 'column', justifyContent: 'space-between', padding: 20, margin: 5, }}>
-          <Temporizador />
-          <Cronometro />
-        </View>
+      <View style={{ flexDirection: 'column', justifyContent: 'space-between', padding: 20, margin: 5, }}>
+        <Temporizador />
+        <Cronometro />
+      </View>
 
-      </SafeAreaView>
-    </Suspense>
+    </SafeAreaView>
+    // </Suspense>
   );
 }
 
@@ -113,4 +180,9 @@ const styles = StyleSheet.create({
     width: 60,
     borderRadius: 5,
   },
+  btnTimer: {
+    height: '60%', 
+    width: '60%', 
+    borderRadius: 6,
+  }
 })
